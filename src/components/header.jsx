a039b93f66c5cc8353a32457ed662ad1a218c2fb
unknown <thonghm20@uef.edu.vn>
2024-06-03 16:37:36 +0700

    sad

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,22 +11,15 @@ import { MdClose } from "react-icons/md";
 
 const Header = () => {
   const [openMenu, setOpenMenu] = useState(false)
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 781);
-
+  
   const toggleMenu = () =>{
     setOpenMenu((open) => !open)
   }
   const closeMenu = () => {
     setOpenMenu(false);
   };
-  const handleResize = () => {
-    setIsMobile(window.innerWidth >= 781);
-  };
+ 
 
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
 
   return (
     <div className="header">
@@ -36,20 +29,20 @@ const Header = () => {
         </Link>
       </div>
       <div class={`list ${openMenu ? 'is-open' : ''}`}>
-        <Link to='/aboutus' className="link"><span>Về chúng tôi</span></Link>
-        <Link to='/services' className="link">Dịch Vụ Và Giải Pháp</Link>
-        <Link className="link">Tư Vấn Doanh Nghiệp</Link>
-        <Link className="link">Thuế</Link>
-        <Link className="link">Cách ngành nghề</Link>
-        <Link to='/contact' className="link">Liên hệ</Link>
+        <Link onClick={closeMenu} to='/aboutus' className="link"><span>Về chúng tôi</span></Link>
+        <Link onClick={closeMenu} to='/services' className="link">Dịch Vụ Và Giải Pháp</Link>
+        <Link onClick={closeMenu} to='/detail' className="link">Tư Vấn Doanh Nghiệp</Link>
+        <Link onClick={closeMenu} className="link">Thuế</Link>
+        <Link onClick={closeMenu} className="link">Cách ngành nghề</Link>
+        <Link onClick={closeMenu} to='/contact' className="link">Liên hệ</Link>
         <MdClose  onClick={toggleMenu} size={30} className="close"/>
       </div>
       <div className="action">
-        <CiSearch size={30} fontWeight='900' />
+        <CiSearch size={30} fontWeight='900'/>
         <img src={gb} alt="" />
       <CiMenuBurger onClick={toggleMenu} className="menu" size={30} color="white"/>
       </div>
-      {openMenu && !isMobile && <div className="overlay is-visible" onClick={toggleMenu}></div>}
+      {openMenu && <div className="overlay is-visible" onClick={toggleMenu}></div>}
     </div>
     
   );
